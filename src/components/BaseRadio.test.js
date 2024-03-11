d2696ae768f8f9e505214e0fcedcfa4af96e5289
Yahor Yankavets <mr.yankovec@gmail.com>
2024-03-11 23:52:43 +0300

    update tests and added new tests

---
+++ src/components/BaseRadio.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/vue";
+import BaseRadio from "./BaseRadio.vue";
+
+const label = "Label";
+const value = "value";
+
+function renderRadio(isChecked) {
+  const options = {
+    props: {
+      modelValue: isChecked ? value : null,
+      value,
+      id: "id",
+    },
+    slots: {
+      default: label,
+    },
+  };
+
+  return render(BaseRadio, options);
+}
+
+//tests ==========================================================================
+
+it("renders checked with label", () => {
+  const isChecked = true;
+
+  renderRadio(isChecked);
+
+  expect(screen.getByLabelText(label).checked).toBe(isChecked);
+});
+
+it("renders unchecked with label", () => {
+  const isChecked = false;
+
+  renderRadio(isChecked);
+
+  expect(screen.getByLabelText(label).checked).toBe(isChecked);
+});
+
+it("renders with value", () => {
+  renderRadio();
+
+  expect(screen.getByLabelText(label).value).toBe(value);
+});
---
+++ src/components/BaseTextarea.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/vue";
+import BaseTextarea from "./BaseTextarea.vue";
+
+function renderTextarea(text, limit) {
+  const options = {
+    props: {
+      modelValue: text,
+      limit,
+    },
+  };
+
+  return render(BaseTextarea, options);
+}
+
+//tests ==========================================================================
+
+it("renders prefilled", () => {
+  const text = "Dummy text";
+
+  renderTextarea(text);
+
+  screen.getByDisplayValue(text);
+});
+
+it("shows counter", () => {
+  const text = "Dummy text";
+  const limit = 100;
+
+  renderTextarea(text, limit);
+
+  screen.getByText(`${text.length} / ${limit}`);
+});
